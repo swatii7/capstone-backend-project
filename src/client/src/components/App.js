ae0a9ff6c35d07e255a2132de912b09349ae804e
swatii7 <swatic946@gmail.com>
2023-05-01 21:59:46 +0530

    make backend and do validations

--- src/client/src/components/App.js
+++ src/client/src/components/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import '../styles/App.css';
 import '../styles/bootstrap.min.css'
+import MainPage from "../layout/MainPage";
 
 const App = () => {
 
   // write your code here
-  return (<></>);
+  return (
+    <>
+      <MainPage />
+    </>
+  );
 }
 
 
