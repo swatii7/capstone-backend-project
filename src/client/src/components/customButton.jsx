4f1507d9f279105f82d62ded147ab51c5f0d709b
swatii7 <swatic946@gmail.com>
2023-05-01 17:08:06 +0530

    make backend and do some validations

---
+++ src/client/src/components/customButton.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+export default function customButton() {
+  return (
+    <div>
+        <Button variant="outline-dark">Dark</Button>
+    </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ const express = require("express");
 const app = express();  //constants
 const bodyParser = require("body-parser");   //core module
 const port = 8080;
-const path = require('path')
+
+const path = require('path');
+const {connection } = require('./connector');
+
 
 // Body Parser middleware to parse request bodies
 app.use(
@@ -11,15 +14,72 @@ app.use(
         { extended: false }));
 
 app.use(bodyParser.json());
-
-const { connection } = require("./connector");
+const router = express.Router();
 const cors = require('cors')  // Third party module
 app.use(cors())
+
+
+//get api for orders
+app.get('/api/bookings', function(req, res) {
+    try {
+
+      //get last booking 
+      connection.findOne({}, {}, { sort: { _id: -1 } }, (error, data) => {
+        if (error) {
+          //it there is no data return message
+          res.status(200).json ({message:"No Previous booking found"});
+        } else {
+            console.log(data);
+            //respond send 
+            res.status(200).json ({data:data});
+        }
+    });
+      
+      
+    } catch (err) {
+      console.error(err);
+      res.status(500).json ({error:"BInternal Server Error"});
+    }
+})
+
+//post api for bookings insert data
+app.post('/api/bookings', function(req, res) {
+
+  const {movie, slot, seats} = req.body;  //destructing
+  //validation 
+  if(!movie || !slot || !seats ){
+    res.status(400).json ({error:"Invalid Requst, Please select a movie, slot and seats"});
+  }
+  else{
+
+  let newDocument = new connection({
+    movie: req.body.movie,
+    slot: req.body.slot,
+    seats: req.body.seats
+});
+
+    try {
+      newDocument.save((error) => {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Document inserted successfully!');
+            res.status(200).json ({message:"Booking Successfully"});
+        }
+    });
+  }
+
+    catch (err) {
+    console.error(err);
+    res.status(500).send('Internal server error');
+  }
+}
+})
+
 
 
 // Start server
 app.listen(port, () => console.log(`App listening on port ${port}!`));
 
-app.use("/api/booking")
 
 module.exports = app;   
