b579b8e60c7ce7c97cb499862d096dce965654b9
swatii7 <swatic946@gmail.com>
2023-06-03 09:27:02 +0530

    finalize

--- client/src/axiosConfig.js
+++ client/src/axiosConfig.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
+console.log( window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'? process.env.REACT_APP_API_LOCAL_PATH: process.env.REACT_APP_API_LIVE_PATH, // Set your default base URL here condition is imosed on live and local port
+)
+
 
 const instance = axios.create({
     baseURL: window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'? process.env.REACT_APP_API_LOCAL_PATH: process.env.REACT_APP_API_LIVE_PATH, // Set your default base URL here condition is imosed on live and local port
